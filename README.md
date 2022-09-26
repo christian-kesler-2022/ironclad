2f6c62c4e0ecdaaa17f8e6360818036ec0ead0da
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-09-26 08:48:20 -0700

    Rename to Ironclad

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# Ironclad_2
+# Ironclad
 
 ## Demo Commands
 
         docker run -d \
         -e MYSQL_ROOT_PASSWORD=secret \
-        -e MYSQL_DATABASE=ironclad_2 \
+        -e MYSQL_DATABASE=ironclad \
         mysql:5.7
-        mkdir ironclad_2
-        cd ironclad_2
+        mkdir ironclad
+        cd ironclad
         git init
         git branch -m main
-        git remote add origin https://github.com/christian-kesler-2022/ironclad_2
+        git remote add origin https://github.com/christian-kesler-2022/ironclad
         git pull origin main
-        docker build . -t ironclad_2
+        docker build . -t ironclad
         docker run -it \
         -e MYSQL_ROOT_PASSWORD=secret \
-        -p 3000:3000 ironclad_2
+        -p 3000:3000 ironclad
 
 ## Dependencies
 
--- src/utils/sqlt.js
+++ src/utils/sqlt.js
@@ -6,12 +6,12 @@ var sqlt = {
       host: '172.17.0.2',
       user: 'root',
       password: 'secret',
-      database: `ironclad_2`,
+      database: `ironclad`,
     });
 
-    // con.query('CREATE SCHEMA `ironclad_2`;');
+    // con.query('CREATE SCHEMA `ironclad`;');
     con.query(
-      'CREATE TABLE `ironclad_2`.`accounts` ( `id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NULL, `username` VARCHAR(255) NULL, `hash` VARCHAR(255) NULL, `salt` VARCHAR(255) NULL, `reset_key` VARCHAR(255) NULL, `nickname` VARCHAR(255) NULL, `pfp` VARCHAR(255) NULL, `theme` VARCHAR(255) NULL, `created_at` DATETIME NULL, `updated_at` DATETIME NULL, `deleted_at` DATETIME NULL, PRIMARY KEY (`id`));'
+      'CREATE TABLE `ironclad`.`accounts` ( `id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NULL, `username` VARCHAR(255) NULL, `hash` VARCHAR(255) NULL, `salt` VARCHAR(255) NULL, `reset_key` VARCHAR(255) NULL, `nickname` VARCHAR(255) NULL, `pfp` VARCHAR(255) NULL, `theme` VARCHAR(255) NULL, `created_at` DATETIME NULL, `updated_at` DATETIME NULL, `deleted_at` DATETIME NULL, PRIMARY KEY (`id`));'
     );
     return con;
   },
