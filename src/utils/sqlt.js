e268794577c4446ecd8df45c6d6afc5f41900ad5
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-09-26 08:05:53 -0700

    1 file modified

--- src/utils/sqlt.js
+++ src/utils/sqlt.js
@@ -9,6 +9,21 @@ var sqlt = {
       database: `ironclad_2`,
     });
 
+    con.query(`CREATE SCHEMA `ironclad` ;
+    CREATE TABLE `ironclad`.`accounts` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NULL,
+    `username` VARCHAR(255) NULL,
+    `hash` VARCHAR(255) NULL,
+    `salt` VARCHAR(255) NULL,
+    `reset_key` VARCHAR(255) NULL,
+    `nickname` VARCHAR(255) NULL,
+    `pfp` VARCHAR(255) NULL,
+    `theme` VARCHAR(255) NULL,
+    `created_at` DATETIME NULL,
+    `updated_at` DATETIME NULL,
+    `deleted_at` DATETIME NULL,
+    PRIMARY KEY (`id`));`);
     return con;
   },
 };
