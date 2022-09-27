65b9ff0bca4e915607e67fcbbfa34d3b54ebebfd
Chris <christian.j.kesler@gmail.com>
2022-09-27 08:39:04 -0700

    Handle ips

--- src/utils/sqlt.js
+++ src/utils/sqlt.js
@@ -2,17 +2,30 @@ const mysql = require('mysql');
 
 var sqlt = {
   initialize: function () {
+    // Connect to db on labs.play-with-docker
+    // var con = mysql.createConnection({
+    //   host: '172.17.0.2',
+    //   user: 'root',
+    //   password: 'secret',
+    //   database: `ironclad`,
+    // });
+
+    // Connect to db on local podman container
     var con = mysql.createConnection({
-      host: '172.17.0.2',
+      host: '10.88.0.2',
       user: 'root',
       password: 'secret',
       database: `ironclad`,
     });
 
     // con.query('CREATE SCHEMA `ironclad`;');
-    con.query(
-      'CREATE TABLE `ironclad`.`accounts` ( `id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NULL, `username` VARCHAR(255) NULL, `hash` VARCHAR(255) NULL, `salt` VARCHAR(255) NULL, `reset_key` VARCHAR(255) NULL, `nickname` VARCHAR(255) NULL, `pfp` VARCHAR(255) NULL, `theme` VARCHAR(255) NULL, `created_at` DATETIME NULL, `updated_at` DATETIME NULL, `deleted_at` DATETIME NULL, PRIMARY KEY (`id`));'
-    );
+    try {
+      con.query(
+        'CREATE TABLE IF NOT EXISTS `ironclad`.`accounts` ( `id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NULL, `username` VARCHAR(255) NULL, `hash` VARCHAR(255) NULL, `salt` VARCHAR(255) NULL, `reset_key` VARCHAR(255) NULL, `nickname` VARCHAR(255) NULL, `pfp` VARCHAR(255) NULL, `theme` VARCHAR(255) NULL, `created_at` DATETIME NULL, `updated_at` DATETIME NULL, `deleted_at` DATETIME NULL, PRIMARY KEY (`id`));'
+      );
+    } catch (error) {
+      console.log(error)
+    }
     return con;
   },
 };
