0521fdfb247d556e86ba7a9c08785a42cf29770d
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-10-03 09:53:36 -0700

    enable docker labs ip

--- src/utils/sqlinit.js
+++ src/utils/sqlinit.js
@@ -3,20 +3,20 @@ const mysql = require('mysql');
 var sqlt = {
   initialize: function () {
     // Connect to db on labs.play-with-docker
-    // var con = mysql.createConnection({
-    //   host: '172.17.0.2',
-    //   user: 'root',
-    //   password: 'secret',
-    //   database: `ironclad`,
-    // });
-
-    // Connect to db on local podman container
     var con = mysql.createConnection({
-      host: '10.88.0.2',
+      host: '172.17.0.2',
       user: 'root',
       password: 'secret',
       database: `ironclad`,
     });
+
+    // Connect to db on local podman container
+    // var con = mysql.createConnection({
+    //   host: '10.88.0.2',
+    //   user: 'root',
+    //   password: 'secret',
+    //   database: `ironclad`,
+    // });
 
     // con.query('CREATE SCHEMA `ironclad`;');
     try {
@@ -24,7 +24,7 @@ var sqlt = {
         'CREATE TABLE IF NOT EXISTS `ironclad`.`accounts` ( `id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NULL, `username` VARCHAR(255) NULL, `hash` VARCHAR(255) NULL, `salt` VARCHAR(255) NULL, `reset_key` VARCHAR(255) NULL, `nickname` VARCHAR(255) NULL, `pfp` VARCHAR(255) NULL, `theme` VARCHAR(255) NULL, `created_at` DATETIME NULL, `updated_at` DATETIME NULL, `deleted_at` DATETIME NULL, PRIMARY KEY (`id`));'
       );
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
     return con;
   },
